eb2bca1d63fd53cfa5c1efa10b355ba793875760
n-murphy <38255714+n-murphy@users.noreply.github.com>
2022-10-04 17:51:58 +0100

    add config for kubectl

--- WEEK1/init-k8s.sh
+++ WEEK1/init-k8s.sh
@@ -23,7 +23,11 @@ msg "initializing the cluster"
 kubeadm init --apiserver-advertise-address $IP_ADDR --apiserver-cert-extra-sans controlplane --pod-network-cidr 10.244.0.0/16
 
 if (( $? == 0 )) ; then
-  msg "cluster initialization was successful applying the flannel configuration"
+  msg "cluster initialization was successful proceeding with setting up config for using kubectl"
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
   # apply the pod network
+  msg "applying the flannel configuration"
   kubectl apply -f ${HOME}/flannel.yaml
 fi
