3a058d1ea569f59d82c7ca7bb996250fcecacc50
n-murphy <38255714+n-murphy@users.noreply.github.com>
2022-10-04 18:02:13 +0100

    update init script to test on killercoda

--- WEEK1/init-k8s.sh
+++ WEEK1/init-k8s.sh
@@ -22,6 +22,11 @@ fi
 msg "initializing the cluster"
 kubeadm init --apiserver-advertise-address $IP_ADDR --apiserver-cert-extra-sans controlplane --pod-network-cidr 10.244.0.0/16
 
+# For testing on killercoda due to resource limitations and socket conflicts ie more than one to choose from you need to add:
+# --cri-socket unix:///var/run/cri-dockerd.sock (as docker.sock already exists)
+# --ignore-preflight-errors=NumCPU (Only 1 CPU available so need to turn this off.)
+#kubeadm init --apiserver-advertise-address $IP_ADDR --apiserver-cert-extra-sans controlplane --pod-network-cidr 10.244.0.0/16 --cri-socket unix:///var/run/cri-dockerd.sock --ignore-preflight-errors=NumCPU
+
 if (( $? == 0 )) ; then
   msg "cluster initialization was successful proceeding with setting up config for using kubectl"
   mkdir -p $HOME/.kube
